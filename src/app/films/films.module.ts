ac4bafff8f8ca37d16dc88dbd0bce0ade2168f8c
Julio Rossell <juliorosselltapia@gmail.com>
2022-05-17 22:32:30 -0500

    fix: removing unused card element

--- src/app/films/films.module.ts
+++ src/app/films/films.module.ts
@@ -5,7 +5,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button'
 
 import { FilmsComponent } from './films.component';
-import { CardModule } from '../shared/components/card/card.module';
 import { FilmsRoutingModule } from './films-routing';
 import { FilmDetailsComponent } from './film-details/film-details.component';
 import { CharacterDetailComponent } from './character-detail/character-detail.component';
@@ -14,7 +13,6 @@ import { CharacterDetailComponent } from './character-detail/character-detail.co
   imports: [
     CommonModule,
     FilmsRoutingModule,
-    CardModule,
     MatCardModule,
     MatIconModule,
     MatButtonModule
